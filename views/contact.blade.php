82e8cc2753ddee5960d93112d3dae56a5b2778bb
Matekyy <54098591+Matekyy@users.noreply.github.com>
2019-08-30 16:25:31 +0800

    Add files via upload

---
+++ views/contact.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('content')
+<h1>This is a Laravel Contact Webpage</h1>
+@endsection
---
+++ views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('content')
+<h1>This is a Laravel Home Webpage</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error sequi quia, maiores molestias blanditiis ducimus, repudiandae quo delectus culpa veniam architecto. Explicabo ullam aspernatur aperiam ducimus, delectus cumque minima? Voluptatem.</p>
+@endsection
+
+@section('sidebar')
+@endsection
